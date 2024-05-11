8dcb373e7c139c288607196f5af605c84a2ca87b
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2024-05-11 10:17:55 +0200

    Clean up script

--- scripts/08_google_trends.R
+++ scripts/08_google_trends.R
@@ -1,59 +1,30 @@
 
-# Plot -----------------------------------------------------------
-
-ggplot(gt_climate_change_2004_2022, aes(x = Month, y = climate_change)) +
-  geom_point() + geom_line() +
-  theme_minimal()
-
-ggplot(gt_climate_change_2004_2024, aes(x = Month, y = climate_change)) +
-  geom_point() + geom_line() +
-  theme_minimal()
-
-
 # Re-Index -----------------------------------------------------------
 
+# Remove missing values at start of timeline
+gt_climate_change_2004_2022 <- gt_climate_change_2004_2022 %>% dplyr::filter(Month > "2007-02-01")
+gt_climate_change_2004_2024 <- gt_climate_change_2004_2024 %>% dplyr::filter(Month > "2007-02-01")
 
-df_2004_2022 <- gt_climate_change_2004_2022 %>% dplyr::filter(Month > "2007-02-01")
-df_2004_2024 <- gt_climate_change_2004_2024 %>% dplyr::filter(Month > "2007-02-01")
+# "Lift up" values to have same starting point
+gt_climate_change_2004_2024$lifted <- gt_climate_change_2004_2024$climate_change + (gt_climate_change_2004_2022$climate_change[1] - gt_climate_change_2004_2024$climate_change[1])
 
-ggplot(df_2004_2022, aes(x = Month, y = climate_change)) +
-  geom_point() + geom_line() +
-  ylim(0, 100) +
-  theme_minimal()
+# Calculate index compared to first value
+gt_climate_change_2004_2024$index <- gt_climate_change_2004_2022$climate_change[1] / gt_climate_change_2004_2024$lifted
 
-ggplot(df_2004_2024, aes(x = Month, y = climate_change)) +
-  geom_point() + geom_line() +
-  theme_minimal()
+# Calculate new climate change value
+gt_climate_change_2004_2024$climate_change_new <- gt_climate_change_2004_2024$lifted / gt_climate_change_2004_2024$index
 
-first_value_2004_2022 <- df_2004_2022$climate_change[1]
-first_value_2004_2024 <- df_2004_2024$climate_change[1]
-df_2004_2022$index <- first_value_2004_2022 / df_2004_2022$climate_change
+# Replace values bigger than 100 with 100
+gt_climate_change_2004_2024 <- gt_climate_change_2004_2024 %>% 
+  mutate(climate_change_new = case_when(climate_change_new > 100 ~ 100,
+                                        T ~ climate_change_new))
 
-df_2004_2024$cor_1 <- df_2004_2024$climate_change + (first_value_2004_2022 - first_value_2004_2024)
-
-ggplot(df_2004_2024, aes(x = Month, y = cor_1)) +
-  geom_point() + geom_line() +
-  theme_minimal()
-
-# df_2004_2024 <- df_2004_2024 %>% left_join(select(df_2004_2022, Month, index), by = "Month")
-
-df_2004_2024$index <- first_value_2004_2022 / df_2004_2024$cor_1
-
-# df_2004_2024$cor_2 <- df_2004_2024$cor_1 / df_2004_2024$index
-
-df_2004_2024$climate_change_new <- df_2004_2024$cor_1 / df_2004_2024$index
-
-ggplot(df_2004_2024, aes(x = Month, y = climate_change_new)) +
-  geom_point() + geom_line() +
-  ylim(0, 100) +
-  theme_minimal()
-
-
+# Sanity plot
 ggplot() +
-  # geom_line(data = df_2004_2022, aes(x = Month, y = climate_change), color = "red") +
-  # geom_point(data = df_2004_2022, aes(x = Month, y = climate_change), color = "red") +
-  geom_line(data = df_2004_2024, aes(x = Month, y = climate_change_new)) +
-  geom_point(data = df_2004_2024, aes(x = Month, y = climate_change_new)) +
+  geom_line(data = gt_climate_change_2004_2022, aes(x = Month, y = climate_change), color = "red") +
+  geom_point(data = gt_climate_change_2004_2022, aes(x = Month, y = climate_change), color = "red") +
+  geom_line(data = gt_climate_change_2004_2024, aes(x = Month, y = climate_change_new)) +
+  geom_point(data = gt_climate_change_2004_2024, aes(x = Month, y = climate_change_new)) +
   ylim(0, 100) +
   theme_minimal()
 

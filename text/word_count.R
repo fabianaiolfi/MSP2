d3ab59567f9ccb9d42e98e9c6ee57bd5f627dfd9
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2024-05-19 17:24:56 +0200

    Update paper

--- text/word_count.R
+++ text/word_count.R
@@ -74,7 +74,7 @@ folder_path <- "text/"
 total_words <- count_words_in_rmd(folder_path)
 cat("Total words in all Rmd files:", total_words)
 
-# 240519: 7246
+# 240519: 7259
 # 240518: 6296
 # 240517: 5693
 # 240514: 4556
